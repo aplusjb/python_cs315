59dd75c03226f04e0b7b907f867d792ea2180222
aplusjb <97636818+aplusjb@users.noreply.github.com>
2025-05-12 21:00:00 -0500

    Added WHERE bin=1 to relevant queries

--- sql/lbin_by_rarity.sql
+++ sql/lbin_by_rarity.sql
@@ -1,5 +1,6 @@
 WITH lbin AS (SELECT item_name, MIN(start_bid) AS lowest
                     FROM Skyblock.Auction
+                    WHERE bin = 1
                     GROUP BY item_name)
 SELECT a.rarity, AVG(lbin.lowest)
 FROM lbin NATURAL JOIN Skyblock.Auction AS a
--- sql/lowest_bin.sql
+++ sql/lowest_bin.sql
@@ -1,3 +1,4 @@
 SELECT item_name, MIN(start_bid), COUNT(*)
 FROM Skyblock.Auction
+WHERE bin = 1
 GROUP BY item_name;
